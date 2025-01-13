bdf6a00da49a5324223d8106d8c1c14cbf2738ac
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2025-01-13 16:08:55 -0600

    extending classes

--- src/test.ts
+++ src/test.ts
@@ -49,44 +49,90 @@ console.log(b["def"]);
 //     " steps"
 // );
 
-interface IAnimal {
+// interface IAnimal {
+//   name: string;
+//   age: number;
+
+//   feed(food: string, amount: number): void;
+// }
+
+// class Cat implements IAnimal {
+//   name: string;
+//   age: number;
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   feed(food: string, amount: number): void {
+//     console.log(
+//       "Feeding " + this.name + " the Cat " + amount + " kg of " + food
+//     );
+//   }
+// }
+
+// class Dog implements IAnimal {
+//   name: string;
+//   age: number;
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   feed(food: string, amount: number): void {
+//     console.log(
+//       "Feeding " + this.name + " the Dog " + amount + " kg of " + food
+//     );
+//   }
+// }
+
+// const CAT = new Cat("Cosmo", 8);
+// const DOG = new Dog("Rusty", 12);
+// CAT.feed("Fish", 0.1);
+// DOG.feed("Beef", 0.25);
+
+class Animal {
   name: string;
   age: number;
-
-  feed(food: string, amount: number): void;
-}
-
-class Cat implements IAnimal {
-  name: string;
-  age: number;
-
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
+
   feed(food: string, amount: number): void {
     console.log(
-      "Feeding " + this.name + " the Cat " + amount + " kg of " + food
+      "Feeding " +
+        this.name +
+        " the " +
+        this.constructor.name +
+        " " +
+        amount +
+        " kg of " +
+        food
     );
   }
 }
 
-class Dog implements IAnimal {
-  name: string;
-  age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+class Cat extends Animal {
+  isHungry: boolean;
+  constructor(name: string, age: number, isHungry: boolean) {
+    super(name, age);
+    this.isHungry = isHungry;
   }
+
   feed(food: string, amount: number): void {
-    console.log(
-      "Feeding " + this.name + " the Dog " + amount + " kg of " + food
-    );
+    if (this.isHungry) {
+      super.feed(food, amount);
+    } else {
+      console.log(
+        this.name + " the " + this.constructor.name + " is not hungry"
+      );
+    }
   }
 }
+class Dog extends Animal {}
 
-const CAT = new Cat("Cosmo", 8);
-const DOG = new Dog("Rusty", 12);
+const CAT = new Cat("Cosmo", 8, false);
+const DOG = new Animal("Rusty", 12);
 CAT.feed("Fish", 0.1);
 DOG.feed("Beef", 0.25);
